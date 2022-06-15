3e5dc1e673d3de5bc76cfbc5f78860782370b6a1
Chris Renfrow <dev@chrisrenfrow.me>
2022-06-15 16:48:50 -0700

    A /very/ basic prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// use::rust-gol::Board{..};
+
+use std::{
+    io::{self, Write},
+    thread, time,
+};
+
+pub(crate) fn main() -> io::Result<()> {
+    loop {
+        println!("noteon 64 20 1");
+        io::stdout().flush()?;
+        thread::sleep(time::Duration::from_millis(1000));
+    }
 }

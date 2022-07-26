df326ada1f430537ac11a1abd08b74b41db4e667
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-26 12:40:54 -0700

    Implement board_alive_to_dead_ratio
    Co-authored-by: dwimmertxt <dwimmertxt@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use yagoll::Board;
+use yagoll::{Board, Cell};
 
 use std::error::Error;
 use std::io::Write;
@@ -52,17 +52,34 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let mut board = Board::new_from_file(&config.input_file);
     loop {
         let activity = calc_board_activity(&board);
-        println!("Activity: {}", activity);
-        board.advance_cycle();
+        let ratio = board_alive_to_dead_ratio(&board);
+        println!("Activity: {}\nRatio: {}", activity, ratio);
         println!("{}", board);
+        board.advance_cycle();
         io::stdout().flush()?;
         thread::sleep(time::Duration::from_millis(1000));
     }
 }
 
+/// Return the density/ratio of alive to dead cells for each quadrant
+fn population_density_by_quad(board: &Board) -> (f32, f32, f32, f32) {
+    // Iterate through each quadrant
+    // Calculate alive to dead ratio for each
+
+    (0.0, 0.0, 0.0, 0.0)
+}
+
 /// Return the ratio of alive to dead cells
 fn board_alive_to_dead_ratio(board: &Board) -> f32 {
-    todo!();
+    let mut alive_ct = 0;
+
+    for x in 0..board.width {
+        for y in 0..board.height {
+            alive_ct += if board.get(x, y) == Cell::Alive { 1 } else { 0 }
+        }
+    }
+
+    alive_ct as f32 / (board.width * board.height) as f32
 }
 
 /// Return the "activity level" of the board (number of updates between cycles).
@@ -72,7 +89,9 @@ fn calc_board_activity(board: &Board) -> f32 {
     let width = board.width;
     let height = board.height;
     let mut next = board.clone();
+
     next.advance_cycle();
+
     for x in 0..width {
         for y in 0..height {
             count += if board.get(x, y) != next.get(x, y) {
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use std::{env, process};
 
 pub(crate) fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
     if let Err(e) = gol_midi::run(&config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
